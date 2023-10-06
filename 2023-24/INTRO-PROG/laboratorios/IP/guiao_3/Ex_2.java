a617fd6703b9a12546215ea6d9bcb6434ebe9bae
Omeir Haroon <omeir2404@gmail.com>
2023-10-06 19:29:48 +0100

    saving from vm

---
+++ 2023-24/INTRO-PROG/laboratorios/IP/guiao_3/Ex_2.java
@@ -0,0 +1,14 @@
+public class Ex_2
+{
+    public static void main(String[] args)
+    {
+        int ano = 2023;
+        if (ano % 400 == 0)
+        {
+            System.out.println(ano + "é um ano bissexto");
+        }
+
+        else
+            System.out.println(ano + " nao é um ano bissexto");
+    } 
+}
