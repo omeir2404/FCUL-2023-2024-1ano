6cd94e8e2936025da47b03fdc73ea3154c62506d
Omeir Haroon <omeir2404@gmail.com>
2023-10-06 12:25:46 +0100

    restarting vm

---
+++ 2023-24/INTRO-PROG/laboratorios/IP/guiao_3/Ex_1.java
@@ -0,0 +1,12 @@
+public class Ex_1
+{
+    public static void main(String[] args)
+    {
+        int idade = 19;
+        double altura = 1.81;
+        int i = 1, j = 2,k = 4;        
+
+        System.out.println("idade:" + idade + "\naltura" + altura);
+        System.out.println("\nmedia:" + (i + j + k) / 3);
+    } 
+}
